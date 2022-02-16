e68600321bc679d6b240205b03e07c8cf249b487
Jaiswar8888 <87074705+Jaiswar8888@users.noreply.github.com>
2022-02-16 20:23:38 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,27 @@
+from concurrent.futures.process import _python_exit
+
+from jmespath import search
+
+
+names = []
+phone_numbers = []
+num = 3
+
+for i in range(num):
+    name = input("Name:-----")
+    phone_number = input("Phone Number:----")
+    names.append(name)
+    phone_numbers.append(phone_number)
+print("\nName\t\t\Phone Number")
+
+for i in range(num):
+    print("{}\t\t\t{}".format(names[i],phone_numbers[i]))
+    
+search_term = input("\nEnter Search Term: ")
+print("Search Result")
+if search_term in names:
+     index = names.index(search_term)
+     phone_number = phone_numbers[index]
+     print("Name {}, Phone Number: {}".format(search_term,phone_number))
+else:
+    print("Name not found")
